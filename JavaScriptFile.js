db5e96d3338ef9e73f4d653ec93daef0c1faa75c
Sarker Jawad Islam <isarkerj@gmail.com>
2020-11-29 11:26:48 +0000

    Improved Home page and added some parts for about me and project

---
+++ JavaScriptFile.js
@@ -0,0 +1,12 @@
+// Scrolling function 
+window.onscroll = function () { myFunction() };
+var navbar = document.getElementById("mainNav");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
